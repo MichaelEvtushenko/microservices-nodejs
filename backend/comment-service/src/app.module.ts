1ce7f7d92872103c127027eaa4b4197f6d185cd3
Michael <evtushenkomisha21@gmail.com>
2021-05-10 17:15:16 +0300

    infra: Hashicorp vault config for comment service
    Implement the ability to inject secrets from hashicorp vault server into comment service in k8s cluster

--- backend/comment-service/src/app.module.ts
+++ backend/comment-service/src/app.module.ts
@@ -5,17 +5,7 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
   imports: [
-    TypeOrmModule.forRoot({
-      type: 'postgres',
-      host: 'postgres-svc', // todo: env var; currently set to k8s service name
-      port: 5432,
-      username: 'postgres',
-      password: 'root',
-      database: 'microservices_db',
-      logging: true,
-      entities: ['dist/**/*.entity{.ts,.js}'],
-      synchronize: true, // todo: set to false when deploy to prod
-    }),
+    TypeOrmModule.forRoot(),
     CommentModule,
   ],
   controllers: [InfoController],
